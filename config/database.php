c5f173383710563bcc8e69a2df8057540c77c36b
Hoxton <coxtonhoxton@gmail.com>
2026-07-12 13:41:57 +0200

    Removing sensible credentials from committing

--- config/database.php
+++
@@ -1,7 +0,0 @@
-return [
-    'host' => ' db5020903889.hosting-data.io',
-    'database' => 'hoxfolio',
-    'username' => 'dbu3174509',
-    'password' => 'ABlastFromThePast#1122',
-    'charset' => 'utf8mb4'
-];
